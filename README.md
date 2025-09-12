1fa62e20a195c3712ba6615d11b100b7a15500ad
PierceCappa <cappa.pierce@gmail.com>
2025-09-12 01:47:20 +0000

    Added second get request with queries and path variables

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A gui and service that will allow multiple people to paint together.
 
+Used this guide to help https://medium.com/@anto18671/building-a-clean-api-in-rust-with-actix-web-a-comprehensive-guide-d084e368a988.
+
 # Rust dependencies
 * Tokio - https://crates.io/crates/tokio - Concurrency library.
 * actix - https://crates.io/crates/actix - Used to handle http and websocket connections
--- service/painting_service/src/main.rs
+++ service/painting_service/src/main.rs
@@ -8,21 +8,42 @@ use std::env;
 
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+struct Message {
+    message: String,
 }
 
 
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NumberMessage {
+    number: i32,
+}
 
 
 async fn hello_world() -> HttpResponse
 {
-    HttpResponse::Ok().json("Hello World")
+    let new_message: Message = Message{
+        message: String::from("Hello World"),
+    };
+
+    HttpResponse::Ok().json(new_message)
 }
+
+async fn callAndResponse(message: web::Path<String>, query: web::Query<NumberMessage>) -> HttpResponse
+{
+    let new_message: Message = Message{
+        message: format!("{} and input number is {}", message, query.number),
+    };
+
+    HttpResponse::Ok().json(new_message)
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+
 
     dotenv().ok();
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
@@ -33,7 +54,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             // Define a route: GET requests to "/" will be handled by hello_world
-            .route("/", web::get().to(hello_world))
+            .route("/", web::get().to(hello_world)) //http://localhost:8080/
+            .route("/{message}", web::get().to(callAndResponse)) //http://localhost:8080/hello?number=354886
     })
     // Bind the server to listen on localhost:8080
     .bind(format!("{}:{}", host, port))?
